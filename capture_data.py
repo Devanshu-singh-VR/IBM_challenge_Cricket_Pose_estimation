746c750f77163118d1336dcd49e2b0a13ff72254
prabsaransingh05 <65734600+prabsaransingh05@users.noreply.github.com>
2022-11-10 22:37:43 +0530

    Capture Data And Extract Frame

---
+++ capture_data.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+capture = cv.VideoCapture(0)
+
+fourcc = cv.VideoWriter_fourcc('X', 'V', 'I', 'D')
+videoWriter = cv.VideoWriter('poses/video18.mp4', fourcc, 30.0, (640, 480))
+
+while True:
+    ret, frame = capture.read()
+
+    if ret:
+        cv.imshow('video', frame)
+        videoWriter.write(frame)
+
+    if cv.waitKey(1) == 27:
+        break
+
+capture.release()
+videoWriter.release()
+
+cv.destroyAllWindows()
